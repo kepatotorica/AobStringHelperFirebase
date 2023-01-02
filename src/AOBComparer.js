c6e89611ace8507bd6f2d355c11aa4353ccad191
kepa totorica <34106186+kepatotorica@users.noreply.github.com>
2023-01-02 11:12:01 -0800

    Update AOBComparer.js

--- src/AOBComparer.js
+++ src/AOBComparer.js
@@ -2,68 +2,26 @@ import './AobComparer.css';
 import { useState } from 'react';
 
 function compareAobs(textAreaInput) {
-    const stringList = [];
-    console.log(textAreaInput);
-    console.log(stringList);
-    // while (textAreaInput.hasNextLine()) {
-    //     const mine = textAreaInput.nextLine();
-    //     if (mine.length() !== 0) {
-    //         const comp = mine.charAt(0);
-    //     }
-    //     let result = '';
-    //     let resultWithSlashes = '\\x';
-    //     let mask = '';
-    //     let maskVal = 'x';
-    //     let writeMask = true;
+    var listOfInputs = textAreaInput.trim().split("\n");
+    let aobList = listOfInputs.map(string => string.split(' '));
+    let AOB = aobList[0];
+    aobList.forEach(list => {
+        list.forEach((aobElement, index) => {
+            if (AOB[index] !== aobElement) {
+                AOB[index] = '?';
+            }
+        });
+    });
 
-    //     for (let i = 0; i < stringList[0].length(); i++) {
-    //         let comp = stringList[0].charAt(i);
-    //         for (const s of stringList) {
-    //             if (s.charAt(i) !== comp || comp === '?') {
-    //                 comp = '?';
-    //                 maskVal = '?';
-    //             }
-    //         }
-    //         result += comp;
-    //         if (comp === ' ') {
-    //             mask += maskVal;
-    //             maskVal = 'x';
-    //             resultWithSlashes += '\\x';
-    //         } else {
-    //             if (comp === '?') {
-    //                 resultWithSlashes += 'a'; // this doesn't matter because of the mask
-    //             } else {
-    //                 resultWithSlashes += comp;
-    //             }
-    //         }
-    //     }
-
-    //     console.log(result);
-    //     // console.log(resultWithSlashes);
-    //     // console.log(mask);
-    //     console.log('\n\nto paste into your prog');
-    //     if (resultWithSlashes.charAt(resultWithSlashes.length - 1) === 'x') {
-    //         console.log("the x shouldn't be here");
-    //         resultWithSlashes = resultWithSlashes.substring(0, resultWithSlashes.length - 2);
-    //     } else {
-    //         mask += 'x';
-    //     }
-    //     resultWithSlashes += '"';
-    //     // mask += '"';
-    //     // console.error(
-    //     //     `const char * ${fileName}Pattern = "${resultWithSlashes};`,
-    //     // );
-    //     // console.error(`const char * ${fileName}Mask = "${mask};`);
-    // }
-}
+    return AOB;
+};
 
 function AobComparer() {
     const [textInput, setTextInput] = useState('');
 
     function handleChange(event) {
-        compareAobs(event.target.value);
-        //TODOASDF make the setTextInput set to the valid AOBs
-        setTextInput(event.target.value);
+        var AOBString = compareAobs(event.target.value);
+        setTextInput(AOBString.join(' '));
     };
 
     return (
